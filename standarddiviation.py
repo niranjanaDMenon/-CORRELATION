ff7c0c35218a7362ebce99ad69d39cc1e71c126b
niranjanaDMenon <73639547+niranjanaDMenon@users.noreply.github.com>
2021-11-16 03:49:59 -0800

    Add files via upload

---
+++ standarddiviation.py
@@ -0,0 +1,22 @@
+import math
+
+
+
+import csv
+
+with open('class1.csv', newline'') as f:
+    reader = csv.reader(f)
+    file_data = list(reader)
+
+
+file_data.pop(0)
+
+total_marks = 0
+total_entries = len(file_data)
+
+for marks in file_data:
+    total_marks += float(marks[1])
+
+    mean = total_marks / total_entries
+    print("Mean (Average) is ->" str(mean))
+
